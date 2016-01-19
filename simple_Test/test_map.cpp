7607aaf1a66e31b497a3e14a52dd3c40faa227c3
k150030 <ywkim@kaonmedia.com>
2016-01-19 16:53:44 +0900

    enum이 아닌 string으로 변경

--- simple_Test/test_map.cpp
+++ simple_Test/test_map.cpp
@@ -2,25 +2,16 @@
 #include <map>
 #include <string>
 
-enum ETELEFONICA_MODEL
-{
-	eTELEFONICA_MODEL_NA1160,
-	eTELEFONICA_MODEL_NA1160_A1,
-	eTELEFONICA_MODEL_NA1160_A2,
-	eTELEFONICA_MODEL_NA1160_CNT,
-	eTELEFONICA_MODEL_MAX
-};
-
 int main() {
     
-	std::map<ETELEFONICA_MODEL, std::string> TelefonicaModelMap;
-	std::map<ETELEFONICA_MODEL, std::string>::iterator it;
-	TelefonicaModelMap[eTELEFONICA_MODEL_NA1160] 		= "bca_NA1160.bin";
-	TelefonicaModelMap[eTELEFONICA_MODEL_NA1160_A1] 	= "bca_NA1160_A1.bin";
-	TelefonicaModelMap[eTELEFONICA_MODEL_NA1160_A2] 	= "bca_NA1160_A2.bin";
-	TelefonicaModelMap[eTELEFONICA_MODEL_NA1160_CNT] 	= "bca_NA1160_CNT.bin";
+	std::map<std::string, std::string> TelefonicaModelMap;
+	std::map<std::string, std::string>::iterator it;
+	TelefonicaModelMap["NA1160"] 		= "bca_NA1160.bin";
+	TelefonicaModelMap["NA1160-A1"] 	= "bca_NA1160_A1.bin";
+	TelefonicaModelMap["NA1160-A2"] 	= "bca_NA1160_A2.bin";
+	TelefonicaModelMap["NA1160-CNT"] 	= "bca_NA1160_CNT.bin";
 	
-	it = TelefonicaModelMap.find(eTELEFONICA_MODEL_NA1160_CNT);
+	it = TelefonicaModelMap.find("NA1160-A2");
 	
 	printf("%s\n", (it->second).c_str());
 
