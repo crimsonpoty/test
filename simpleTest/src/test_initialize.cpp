2869c6555f866e851bed4474665b0cbe41cc5dd6
k150030 <ywkim@kaonmedia.com>
2016-03-23 11:42:09 -0500

    Add to test of initialize

---
+++ simpleTest/src/test_initialize.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+struct SManualUbList
+{
+	int UbNo;
+	int TxFrequency;
+	int ePolarization;
+	int UbFrequency;
+	int BandWidth;
+	int PowerLevel;
+};
+
+int main() {
+    
+	SManualUbList ManualUbList[2] = {
+		{1, 2, 3, 4, 5, 6},
+		{7, 8, 9, 10, 11, 12}
+	};
+	
+	printf("size array: %d %d\n", sizeof(ManualUbList), sizeof(SManualUbList));
+	
+	for(int i = 0; i < sizeof(ManualUbList) / sizeof(SManualUbList); i++) {
+		printf("%d: %d %d %d %d %d %d\n", i, ManualUbList[i].UbNo, ManualUbList[i].TxFrequency, ManualUbList[i].ePolarization, ManualUbList[i].UbFrequency, ManualUbList[i].BandWidth, ManualUbList[i].PowerLevel);
+	}
+
+	return 0;
+}
