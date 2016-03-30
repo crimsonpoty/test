6c4e8c884d7113d920a8aaa75f5b07135dbe3b35
k150030 <ywkim@kaonmedia.com>
2016-03-29 20:05:21 -0500

    change compiler to c11 and remove warning

--- simpleTest/src/test_initialize.cpp
+++ simpleTest/src/test_initialize.cpp
@@ -12,14 +12,14 @@ struct SManualUbList
 
 int main() {
     
-	SManualUbList ManualUbList[2] = {
+	SManualUbList ManualUbList[5] = {
 		{1, 2, 3, 4, 5, 6},
-		{7, 8, 9, 10, 11, 12}
+		{7, 8, 9, 10, 11, 12},
 	};
 	
-	printf("size array: %d %d\n", sizeof(ManualUbList), sizeof(SManualUbList));
+	printf("size array: %lu %lu\n", sizeof(ManualUbList), sizeof(SManualUbList));
 	
-	for(int i = 0; i < sizeof(ManualUbList) / sizeof(SManualUbList); i++) {
+	for(int i = 0; i < static_cast<int>(sizeof(ManualUbList) / sizeof(SManualUbList)); i++) {
 		printf("%d: %d %d %d %d %d %d\n", i, ManualUbList[i].UbNo, ManualUbList[i].TxFrequency, ManualUbList[i].ePolarization, ManualUbList[i].UbFrequency, ManualUbList[i].BandWidth, ManualUbList[i].PowerLevel);
 	}
 
