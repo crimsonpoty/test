ae102613a4f0ec5ec253d87dad39daeea1eb00d7
k150030 <ywkim@kaonmedia.com>
2016-04-05 13:44:22 -0500

    test for iterator of vector

--- simpleTest/src/test_vector.cpp
+++ simpleTest/src/test_vector.cpp
@@ -11,10 +11,10 @@ struct test
 	int c;
 };
 
-int main() {
-    
+int main(int argc, char** argv) {
+
 	std::vector<test> Temp;
-	
+
 	test t[] = {
 		{1, 2, 3},
 		{4, 5, 6},
@@ -24,34 +24,34 @@ int main() {
 	for(int i = 0; i < (int)(sizeof(t)/sizeof(test)); i++) {
 		Temp.push_back(t[i]);
 	}
-		
+
 	std::for_each(Temp.begin(), Temp.end(), [] (const test n) {
 		std::cout << n.a << " ";
 		std::cout << n.b << " ";
 		std::cout << n.c <<std::endl;
 	});
-	
+
 	int s1 = 1;
 	int s3 = 3;
 	// std::vector<test>::const_iterator it = std::find_if(Temp.begin(), Temp.end(), [&s1, &s3] (const test n) {
 		// return (n.a == s1) && (n.c == s3);
 	// });
-	
+
 	std::vector<test>::const_iterator it;
 	for(it = Temp.begin(); it != Temp.end(); it++) {
 		if(it->a == s1 && it->c == s3)	break;
 	}
-	
+
 	if(it == Temp.end()) {
 		return -1;
 	}
-	
+
 	std::cout << it->b <<std::endl;
-	
-	std::cout << std::endl << std::endl << std::endl << "=========================================" << std::endl;
-	
+
+	std::cout << std::endl << std::endl << "=========================================" << std::endl << std::endl;
+
 	Temp.clear();
-	
+
 	test iterTest[] = {
 		{0, 1, 2},
 		{1, 1, 2},
@@ -63,13 +63,32 @@ int main() {
 		{7, 1, 2},
 		{8, 1, 2},
 	};
-	
+
 	for(int i = 0; i < (int)(sizeof(iterTest)/sizeof(test)); i++) {
 		Temp.push_back(iterTest[i]);
 	}
-	
+
 	for(auto it = ++Temp.begin(); it != Temp.end(); it+=2) {
 		std::cout << (it+1)->a << std::endl;
+	}
+
+	std::cout << std::endl << std::endl << "=========================================" << std::endl << std::endl;
+
+	if(argc != 2) {
+		std::cout << "Input compare number" << std::endl;
+		return -1;
+	}
+
+	it = Temp.begin();
+	for(; it != Temp.end(); it++) {
+		if(it->a == std::atoi(argv[1])) break;
+	}
+
+	if(it == Temp.end()) {
+		std::cout << "The end" << std::endl;
+	}
+	else {
+		std::cout << it->a << std::endl;
 	}
 
 	return 0;
