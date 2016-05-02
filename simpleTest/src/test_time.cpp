ffafda0df5c77e1792ec60afb1e56d84a6c81f5f
k150030 <ywkim@kaonmedia.com>
2016-05-01 21:48:55 -0500

    add test of localtime

--- simpleTest/src/test_time.cpp
+++ simpleTest/src/test_time.cpp
@@ -1,12 +1,23 @@
 #include <stdio.h>
+#include <time.h>
 
 int main() {
-    
-    unsigned int value = 2366791440;
+	time_t now = (time_t)1449400546;
+	struct tm t;
 
-    int checksum = (((value / 100) % 23) + (value % 100)) % 100;
+	// UTC를 국가 시간으로 변환
+    t = *localtime(&now);
 
-    printf("checksum = %d\n", checksum);
+	// 1970년 1월 1일 0시를 기준.
+	printf("현재 국가 시간 : %d/%02d/%02d %02d:%02d:%02d\n",
+		t.tm_year + 1900, t.tm_mon + 1, t.tm_mday,
+		t.tm_hour, t.tm_min, t.tm_sec);
+
+	t = *gmtime(&now);
+
+	printf("세계 표준 시간 : %d/%02d/%02d %02d:%02d:%02d\n",
+		t.tm_year + 1900, t.tm_mon + 1, t.tm_mday,
+		t.tm_hour, t.tm_min, t.tm_sec);
 
 	return 0;
 }
