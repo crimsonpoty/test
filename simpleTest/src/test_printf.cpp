82f141926c90c75cca1a73b2a0f61061b286b97d
k150030 <ywkim@kaonmedia.com>
2016-06-07 19:46:11 -0500

    2바이트 문자 출력

--- simpleTest/src/test_printf.cpp
+++ simpleTest/src/test_printf.cpp
@@ -2,12 +2,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string>
 
 // #define TTXOSD_DEBUG
 // #define printf(x, ...)
 // #define printf(...)
 
-int main() {
+int main(int argc, char* argv[]) {
 
 	// char Name[]="Kim Sang Hyung";
 
@@ -30,10 +31,22 @@ int main() {
     //unsigned char zoneId[3] = "15";
     //printf("zoneId(%s)\n", zoneId);
     
-    char chip_id[12] = "00694967295";
-    unsigned long int Num = atoi(chip_id);
-    
-    printf("%s, %0lu, %X\n", chip_id, Num, (int)Num);
+	// 한글 출력
+	std::string Text("한글 출력");
+	printf("%s\n", Text.c_str());
+	Text.append(" append");
+	printf("%s\n", Text.c_str());
+
+	int test = 0;
+	
+	for(; test < 6; test++) {
+		if(test == atoi(argv[1])) break;
+	}
+	printf("for test: %d\n", test);
+	
+	// 스페인어 출력
+	unsigned char spanChar[2] = {0xC3, 0x91};
+	printf("%s\n", std::string((const char *)spanChar).c_str());
 
 	return 0;
 }
