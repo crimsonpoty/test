fd38eff59ca000b8763bf08704594f2a049c7e6e
crimson_88 <crimson_88@naver.com>
2017-09-04 18:15:34 +0900

    스도쿠 c++ 모듈(CSudoku) 파이썬 프로젝트 폴더로 복사하고 컴파일 하도록 배치 파일 수정

--- python/SudokuSolver/src/main/cpp/command_Sudoku.bat
+++ python/SudokuSolver/src/main/cpp/command_Sudoku.bat
@@ -1,15 +1,20 @@
 rem @echo off
-rem assume that source file(Sudoku.cpp) already was compiled in SRC_PATH.
 
-set OBJ_PATH="C:\Users\crims\workspace\repo\git\practice\sudoku_solution\Debug\src"
 set SRC_PATH="C:\Users\crims\workspace\repo\git\practice\sudoku_solution\src"
+set OBJ_PATH="%CD%\src"
+set INCLUDE_PATH=%OBJ_PATH%
 set PY_INCLUDE_PATH="C:\Users\crims\AppData\Local\Programs\Python\Python36-32\include"
 set PY_LIB_PATH="C:\Users\crims\AppData\Local\Programs\Python\Python36-32\libs"
 
-REM copy "%OBJ_PATH%\Sudoku.o" .
+if not exist "src" (mkdir "src")
+
+copy "%SRC_PATH%\Sudoku.h" src
+copy "%SRC_PATH%\Sudoku.cpp" src
+
+g++ -std=c++0x -O0 -g3 -Wall -c -fmessage-length=0 -o %OBJ_PATH%"\Sudoku.o" %OBJ_PATH%"\Sudoku.cpp" 
 
 swig -c++ -python Sudoku.i
 
-REM [Windows]
-g++ -c Sudoku_wrap.cxx -fPIC -Wall -I%PY_INCLUDE_PATH% -I%SRC_PATH%
+REM [pyd lib file for Windows]
+g++ -c Sudoku_wrap.cxx -fPIC -Wall -I%PY_INCLUDE_PATH% -I%INCLUDE_PATH%
 g++ -o _cppSudoku.pyd %OBJ_PATH%"\Sudoku.o" Sudoku_wrap.o -Wall -shared -lpython36 -L%PY_LIB_PATH%
